26f74df0e19bcf0b6f14abaad6f9b75704afe3b3
Taryn Orlemann <taryn.orlemann@gmail.com>
2022-08-08 21:18:07 -0700

    Add tests to key spec

---
+++ spec/key_spec.rb
@@ -0,0 +1,24 @@
+
+require './lib/key'
+
+RSpec.describe Key do
+  let(:key) {Key.new}
+
+  it 'exists' do
+    expect(key).to be_a Key
+  end
+
+  it 'has readable attributes' do
+    expect(key.key.count).to eq 5
+  end
+
+  it 'can create keys from key' do
+    expected = {
+      :a => 2,
+      :b => 27,
+      :c => 71,
+      :d => 15
+    }
+    expect(key.key_splitter('02715')).to eq(expected)
+  end
+end
